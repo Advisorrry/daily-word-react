028becf554456b437ebb94db94c5223416f3ee8c
Advisorrry <sypikeh@yandex.com>
2021-12-02 13:55:46 +0500

    add main layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -5,5 +5,8 @@ interface MainLayoutProps {
 }
 
 export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
-  return <div className="text-white font-mono container mx-auto bg-gray-800 h-full text-lg">{children}</div>
+  return <div className="text-white font-mono container mx-auto bg-gray-800 h-full text-lg">
+    <div>{children}</div>
+    <img src={process.env.PUBLIC_URL + 'bg1.png'} className='bg' alt='bg' />
+  </div>
 }
