d813c98be9dd7b0c33a0f413188f64ad7e886ad8
Advisorrry <sypikeh@yandex.com>
2021-12-02 13:56:02 +0500

    add main background

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import styles from './Header.module.scss'
 
+const logo = process.env.PUBLIC_URL + 'logo.png'
+
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = ({}) => {
   return (
     <div className="flex justify-between items-center p-2">
       <div className={styles.logo}>
-        <img className="" src={process.env.PUBLIC_URL + 'logo.png'} alt="logo" />
+        <img className="" src={logo} alt="logo" />
       </div>
       <div className="font-bold font-sans antialiased">Daily Word</div>
       <div>opt</div>
