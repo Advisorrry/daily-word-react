b2a88884ccf6bc84f454fd97cd8823c27c4c9286
Advisorrry <sypikeh@yandex.com>
2021-12-03 18:40:27 +0500

    добавил роуты

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 import styles from './Header.module.scss'
 
 const logo = process.env.PUBLIC_URL + 'logo.png'
+const about = process.env.PUBLIC_URL + 'about.png'
 
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = ({}) => {
   return (
-    <div className="flex justify-between items-center p-2">
-      <div className={styles.logo}>
-        <img className="" src={logo} alt="logo" />
-      </div>
+    <header className="flex justify-between items-center p-2">
+      <Link to="/" className={styles.logo}>
+        <img src={logo} alt="logo" />
+      </Link>
       <div className="font-bold font-sans antialiased">Daily Word</div>
-      <div>opt</div>
-    </div>
+      <NavLink to="/about" className={styles.logo}>
+        <img src={about} alt="logo" />
+      </NavLink>
+    </header>
   )
 }
