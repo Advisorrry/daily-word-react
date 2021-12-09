02f5dc124544703fc9f88d3ba37a9cd2036c4d7a
Advisorrry <sypikeh@yandex.com>
2021-12-09 18:00:27 +0500

    display random word

--- src/hooks/useWords.ts
+++ src/hooks/useWords.ts
@@ -32,6 +32,6 @@ export const useWords = () => {
   }, [])
   return {
     data,
-    isLoading
+    isLoading,
   }
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,14 +5,13 @@ import { useWords } from '../hooks/useWords'
 import { MainLayout } from '../layouts/MainLayout'
 
 export const HomePage: React.FC = () => {
-  const { data } = useWords()
-  console.log(data)
+  const { data, isLoading } = useWords()
 
   return (
     <>
       <MainLayout>
         <Header />
-        <WordsList words={data} />
+        <WordsList words={data} isLoading={isLoading} />
       </MainLayout>
       <img src={process.env.PUBLIC_URL + 'bg1.jpg'} className="mx-auto container" alt="bg" />
     </>
