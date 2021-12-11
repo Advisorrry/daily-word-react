6d645ce85cb0de8a156efaa22ddd4d1a9a11fb2c
Advisorrry <sypikeh@yandex.com>
2021-12-11 19:01:44 +0500

    прокидывание пропсов и отоюражение слов на странице. ps: при переходе на другую страницу WordList перерендывается и слова обновляются(этого быть не должно)

--- src/components/WordsList/index.tsx
+++ src/components/WordsList/index.tsx
@@ -1,38 +1,47 @@
 import clsx from 'clsx'
 import React from 'react'
-import { IWords } from '../../hooks/useWords'
+import { LoadingSkeleton } from '../LoadingSkeleton'
 
 interface WordsListProps {
-  words: IWords[]
+  words: any
   isLoading: boolean
+  randomNumber: number
 }
 
-function selfRandom(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
+export const WordsList: React.FC<WordsListProps> = React.memo(
+  ({ words, isLoading, randomNumber }) => {
+    if (isLoading) {
+      return (
+        <div
+          className={clsx(
+            'max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 ',
+            'layout-height'
+          )}
+        >
+          <LoadingSkeleton />
+        </div>
+      )
+    }
 
-export const WordsList: React.FC<WordsListProps> = ({ words, isLoading }) => {
-
-  const randomWord = selfRandom(0, 2999)
-
-  return (
-    <div
-      className={clsx('max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 ', 'layout-height')}
-    >
-      {!isLoading && (
+    return (
+      <div
+        className={clsx('max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 ', 'layout-height')}
+      >
         <h2 className="font-extrabold w-full tracking-widest text-purple-600 text-4xl ">
           <div className="mb-5 px-1 flex justify-around items-center divide-y divide-fuchsia-300">
-            <span className="text-indigo-700 ">{words[randomWord].en_word}</span>
-            <span className="text-indigo-400 text-lg">lvl: {words[randomWord].level} </span>
+            <span className="text-indigo-700 ">{words[randomNumber].en_word}</span>
+            <span className="text-indigo-400 text-lg">lvl: {words[randomNumber].level} </span>
           </div>
           <div className="flex justify-end px-3">
-            <span className="bg-green-200 rounded-full p-4 text-2xl ">{words[randomWord].ru_word}</span>
+            <span className="bg-green-200 rounded-full p-4 text-2xl ">
+              {words[randomNumber].ru_word}
+            </span>
           </div>
           <span className="block text-indigo-300 text-xs mt-5">
-            часть речи: {words[randomWord].part_speech}
+            часть речи: {words[randomNumber].part_speech}
           </span>
         </h2>
-      )}
-    </div>
-  )
-}
+      </div>
+    )
+  }
+)
