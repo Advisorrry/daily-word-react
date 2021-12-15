64b7899e74c15a1e31c4ae86399c1aa239758db0
Advisorrry <sypikeh@yandex.com>
2021-12-15 16:09:47 +0500

    функция диспачит рандомное число в стейт, однако пока не получается добавить ее, тк она вызывает слишком много ререндеров

--- src/hooks/useRandomValue.ts
+++ src/hooks/useRandomValue.ts
@@ -1,3 +1,10 @@
-export const useRandomValue = (min: number, max: number): number => {
-  return Math.floor(Math.random() * (max - min + 1)) + min
+import { useAppDispatch } from './useRedux'
+import { randomNumberByRange } from '../store/slices/randomNumber'
+
+export const useRandomValue = (min: number, max: number) => {
+  const dispatch = useAppDispatch()
+
+  const _ = Math.floor(Math.random() * (max - min + 1)) + min
+
+  dispatch(randomNumberByRange(_))
 }
