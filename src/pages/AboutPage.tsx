05ec13e444e4f0a9fc5166616bbfc490199a6c0a
Advisorrry <sypikeh@yandex.com>
2021-12-15 16:00:44 +0500

    переделал логику

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,13 +1,5 @@
 import React from 'react'
-import { Header } from '../components/Header'
-import { MainLayout } from '../layouts/MainLayout'
 
 export const AboutPage = () => {
-  return (
-    <>
-      <MainLayout>
-        <Header />
-      </MainLayout>
-    </>
-  )
+  return <>мяу</>
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,27 +1,17 @@
 import React from 'react'
-import { Header } from '../components/Header'
 import { WordsList } from '../components/WordsList'
-import { MainLayout } from '../layouts/MainLayout'
+import { useRandomValue } from '../hooks/useRandomValue'
+import { useAppSelector } from '../hooks/useRedux'
+import { getRandomNumber } from '../selectors'
 import { useGetWordsQuery } from '../store/wordsApi'
 
 export const HomePage: React.FC = () => {
-  const { data, isLoading } = useGetWordsQuery(' ')
-  const [randomNumber, setrandomNumber] = React.useState(0)
-
-  React.useEffect(() => {
-    function selfRandom(min: number, max: number): number {
-      return Math.floor(Math.random() * (max - min + 1)) + min
-    }
-    setrandomNumber(selfRandom(0, 2999))
-  }, [])
+  const { data, isLoading } = useGetWordsQuery('')
+  const { value } = useAppSelector(getRandomNumber)
 
   return (
     <>
-      <MainLayout>
-        <Header />
-        <WordsList words={data} isLoading={isLoading} randomNumber={randomNumber} />
-      </MainLayout>
-      <img src={process.env.PUBLIC_URL + 'bg1.jpg'} className="mx-auto container" alt="bg" />
+      <WordsList words={data} isLoading={isLoading} randomNumber={value} />
     </>
   )
 }
