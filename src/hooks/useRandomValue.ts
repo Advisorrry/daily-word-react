91849dec4dceba269953a79162679e6366bf9c94
Advisorrry <sypikeh@yandex.com>
2021-12-29 20:53:23 +0500

    decompositn func

--- src/hooks/useRandomValue.ts
+++ src/hooks/useRandomValue.ts
@@ -1,10 +1,12 @@
 import { useAppDispatch } from './useRedux'
 import { randomNumberByRange } from '../store/slices/randomNumber'
 
+export const randomValue = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min
+
 export const useRandomValue = (min: number, max: number) => {
   const dispatch = useAppDispatch()
 
-  const _ = Math.floor(Math.random() * (max - min + 1)) + min
+  const _ = randomValue(min, max)
 
   dispatch(randomNumberByRange(_))
 }
